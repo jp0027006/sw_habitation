a74b88044a0b7e51f6ea87e520cd13187e7d75e8
Jay Patel <jp0027006@gmail.com>
2025-02-15 15:04:29 +0530

    removed unwanted files

--- backend/schemaTypes/index.ts
+++ backend/schemaTypes/index.ts
@@ -1,7 +1,3 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
 import navbar from './navbar'
 import footer from './footer'
 
